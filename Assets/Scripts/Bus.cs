6e772fe366a50bd269d26db06e9a5dcaf48da2ab
denycavalli <deny_cavalli@hotmail.com>
2015-07-03 08:34:44 -0300

    Oni: New start
    v0.04 - Changes: Nothing, we starded again.

---
+++ Assets/Scripts/Bus.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bus : MonoBehaviour {
+
+	int i = 1;
+	void Start () {
+	
+	}
+
+	void Update () 
+	{
+		Debug.Log (i);
+		if(Linha.stops != 0)
+		{
+			if(transform.position.x < Rota.pos[i].x)
+			{
+				transform.position += new Vector3(0.01f,0,0);
+			}
+			if(transform.position.z < Rota.pos[i].z)
+			{
+				transform.position += new Vector3(0,0,0.01f);
+			}
+			if(transform.position.x > Rota.pos[i].x)
+			{
+				transform.position += new Vector3(-0.01f,0,0);
+			}
+			if(transform.position.z > Rota.pos[i].z)
+			{
+				transform.position += new Vector3(0,0,-0.01f);
+			}
+			if(transform.position.z > Rota.pos[i+1].z && transform.position.x > Rota.pos[i+1].x)
+			{
+				i++;
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/Custo.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Custo : MonoBehaviour {
+
+	public float cost;
+
+	void Start () 
+	{
+		cost = 5;
+	}
+
+	void Update () 
+	{
+		
+	}
+}

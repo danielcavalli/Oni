1384dd198adf5110595ba8c6dd26a5b53f7a6ba8
Daniel Cavalli <deny_cavalli@hotmail.com>
2015-08-08 23:28:36 -0300

    Revert "Oni Pre-Alpha 0.0.3"
    This reverts commit 74632dbd7cb191d4f79f494e98f75814fa27817c.

---
+++ Assets/Scripts/Spawn.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawn : MonoBehaviour {
+
+	
+
+	void Start() 
+	{
+	
+	}
+
+	void Update() 
+	{
+	
+	}
+}
---
+++ Assets/Scriptsv2/RotaV2.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotaV2 : MonoBehaviour {
+
+
+	void Start () 
+	{
+	
+	}
+	
+
+	void Update () 
+	{
+	
+	}
+}

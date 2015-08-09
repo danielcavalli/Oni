0250676702e286344ac23283bde23d2ef8ca0014
Daniel Cavalli <deny_cavalli@hotmail.com>
2015-08-09 00:07:42 -0300

    ONI Pre-Alpha 0.1v
    We started a Map Editor system to help us in the development and level
    design of this project. In next update we want to finish the Tiles and
    start to optimize the game.

---
+++ Assets/SetGrid.js
@@ -0,0 +1,34 @@
+﻿#pragma strict
+
+
+var grid_x = new Array();
+var grid_y = new Array();
+
+var linha : int = 2;
+var coluna : int = 2;
+
+var grid_type = [];
+
+var T : GameObject;
+
+function grid()
+{
+    for(var i = 0;i < linha;i++)
+    {
+        grid_type[i] = new Array(coluna);
+        for (var n = 0;n < coluna;n++)
+        {
+            grid_x[i] = i;
+            grid_y[n] = n;
+            //grid_type[i][1] = true;
+            if(grid_type[i][n] == true)
+    		{ 
+				Instantiate(T,new Vector3(grid_x[i],0,grid_y[n]),T.transform.rotation);
+			}
+        }
+    }
+}
+grid();
+function Update () 
+{
+}

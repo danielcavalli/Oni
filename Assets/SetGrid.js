e0812cff6b83e774348284a78167bad0fd9d14d1
Daniel Cavalli <deny_cavalli@hotmail.com>
2015-08-09 00:32:04 -0300

    ONI Pre-Alpha 0.1
    We started a Map Editor system to help us in the development and level
    design of this project. In next update we want to finish the Tiles and
    start to optimize the game.

--- Assets/SetGrid.js
+++ Assets/SetGrid.js
@@ -1,5 +1,8 @@
-﻿#pragma strict
-
+﻿//#pragma strict
+import System;
+import System.IO;
+ 
+var  fileName = "MyFile.txt";
 
 var grid_x = new Array();
 var grid_y = new Array();
@@ -7,7 +10,7 @@ var grid_y = new Array();
 var linha : int = 2;
 var coluna : int = 2;
 
-var grid_type = [];
+var grid_type = new Array();
 
 var T : GameObject;
 
@@ -20,15 +23,30 @@ function grid()
         {
             grid_x[i] = i;
             grid_y[n] = n;
-            //grid_type[i][1] = true;
-            if(grid_type[i][n] == true)
-    		{ 
-				Instantiate(T,new Vector3(grid_x[i],0,grid_y[n]),T.transform.rotation);
-			}
+            grid_type[i][n] = false;
         }
     }
 }
 grid();
+/*function Start()
+{
+        if (File.Exists(fileName))
+        {
+            Debug.Log(fileName+" already exists.");
+            return;
+        }
+        var sr = File.CreateText(fileName);
+        for(var i = 0;i < linha;i++)
+	    {
+	        for (var n = 0;n < coluna;n++)
+	        {
+	            sr.WriteLine(" " + grid_type[i][n]);
+	        }
+	    }
+        sr.Close();
+}*/
+//ReadFile(fileName);
 function Update () 
 {
+	
 }
--- Assets/TileSettings.cs
+++ Assets/TileSettings.cs
@@ -1,15 +1,56 @@
 ﻿using UnityEngine;
-using System.Collections;
 
-public class TileSettings : MonoBehaviour {
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Form_Servant;
+using System.Text.RegularExpressions;
+using System.Runtime.InteropServices;
+using System.IO;
+using System.Diagnostics;
 
-	// Use this for initialization
-	void Start () {
+public class TileSettings : MonoBehaviour 
+{
+
+	List<string> user_lines = new List<string> { };
+	string[] userDatas = new string[] { };
 	
+	static public void ReadFile1()
+	{
+		using (StreamReader sr = new StreamReader("MyFile.txt"))
+		{
+			string line;
+			while ((line = sr.ReadLine()) != null)
+			{
+				user_lines.Add(line);
+				string lastLine = sr.ReadLine("MyFile.txt").Last();
+				userDatas = lastLine.Split('|');
+			}
+		}
+		for (int i = 0; i < user_lines.Count; i++)
+		{
+			userDatas = user_lines[i].Split('|');
+			servantBox.DisplayMember = "Name";
+		}
+			
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+	/*void Start()
+	{
+		for(var i = 0;i < linha;i++)
+		{
+			for(var n = 0;n < coluna;n++)
+			{
+				if(grid_type[i][n] != false)
+				{
+					Instantiate(T,new Vector3(grid_x[i],0,grid_y[n]),T.transform.rotation);
+				}
+			}
+		}
+	}*/
 }
